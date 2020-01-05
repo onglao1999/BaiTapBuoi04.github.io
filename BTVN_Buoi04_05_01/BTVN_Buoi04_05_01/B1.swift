15ac350881939ad4e39800b7fe5077056a363cde
Ong_Lao_Ngao <onglaongao@OngLaoNgaosMBP.net.fpt>
2020-01-06 00:58:30 +0700

    bài tập buôit 04

---
+++ BTVN_Buoi04_05_01/BTVN_Buoi04_05_01/B1.swift
@@ -0,0 +1,13 @@
+//
+//  B1.swift
+//  BTVN_Buoi04_05_01
+//
+//  Created by Ong_Lao_Ngao on 1/5/20.
+//  Copyright © 2020 Ong_Lao_Ngao. All rights reserved.
+//1. Tính tổng các chữ số của một số tự nhiên
+
+import Foundation
+func tongSTN(){
+    print("Nhập số tự nhiên từ bàn phím: ", terminator: "")
+    var a: Int = Int(readLine() ?? "0") ?? 0
+}
